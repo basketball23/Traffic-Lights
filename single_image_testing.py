a5b9babca2fc4cb31ffb36bcce2b84ae6881fe8c
Andrew Lou <andrewloupro@gmail.com>
2025-07-18 16:04:57 -0700

    Rearranged project directory and added live testing file

---
+++ single_image_testing.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+model = YOLO("training/traffic_light_detection/lisa_finetune_v1/weights/best.pt")
+
+results = model.predict("test_image_1.webp")
+
+results[0].show()
+results[0].save("test_image_2_results.jpg")
--- testing.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("traffic_light_detection/lisa_finetune_v1/weights/best.pt")
-
-results = model.predict("test_image_1.webp")
-
-results[0].show()
-results[0].save("test_image_2_results.jpg")
